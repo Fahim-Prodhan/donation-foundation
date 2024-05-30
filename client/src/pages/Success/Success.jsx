e4b7c5cc319931fa04547ea963e964dd53e24171
tabrid <mollik15-5096@diu.edu.bd>
2024-05-30 22:07:15 +0600

    donate3

--- client/src/pages/Success/Success.jsx
+++ client/src/pages/Success/Success.jsx
@@ -1,36 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-
 
 function Success() {
     const location = useLocation();
     const query = new URLSearchParams(location.search);
     const transactionId = query.get("session_id");
+    const [fetched, setFetched] = useState(false);
+
     useEffect(() => {
-        fetch("/api/donate/success", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ transactionId }),
-        })
+        const fetchSuccessData = () => {
+            fetch("/api/donate/success", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ transactionId }),
+            })
             .then((res) => res.json())
             .then((data) => {
                 console.log(data);
+                setFetched(true);
             });
-    }, [transactionId]);
+        };
+
+        if (!fetched) {
+            const timeout = setTimeout(fetchSuccessData, 10000); 
+            return () => clearTimeout(timeout);
+        }
+    }, [transactionId, fetched]);
+
     return (
         <div>
             <div className="flex items-center justify-center min-h-screen ">
                 <div className=" p-8 rounded-lg  max-w-md flex flex-col justify-center items-center">
-                   
                     <h2 className="text-2xl font-bold text-center text-gray-800 mb-2">Payment Successful</h2>
                     <p className="text-gray-600 text-center mb-6">Thank you for your Donate</p>
-                    
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Success
+export default Success;
