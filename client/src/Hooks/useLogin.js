d354f456caf66bbcb4f9983d51e7abe9da09a68e
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2025-01-21 04:55:15 +0600

    redirect to dashboard

--- client/src/Hooks/useLogin.js
+++ client/src/Hooks/useLogin.js
@@ -26,7 +26,7 @@ const useLogin = () => {
             toast.success('Successfully Logged in!')
             localStorage.setItem("foundation", JSON.stringify(data));
             setAuthUser(data);
-            navigate(location.state ? `${location.state}` : '/')
+            navigate(location.state ? `${location.state}` : '/admin/dashboard')
             location.reload()
         } catch (error) {
             toast.error(error.message);
